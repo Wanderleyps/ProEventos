9cd787c5ff75e501f2f57fa11f0d5aecfac64a88
Wanderleyps <wanderleyps794@gmail.com>
2023-01-19 21:13:42 -0300

    First commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        
+    }
+}
