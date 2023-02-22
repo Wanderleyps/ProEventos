312476164878ee3f58c5753bce81f41ba498ece4
Wanderleyps <wanderleyps794@gmail.com>
2023-02-22 19:07:03 -0300

    Add domains classes

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ProEventos.API.Models
+namespace ProEventos.Domain
 {
     public class Evento
     {
         public int Id { get; set; }
         public string Local { get; set; }
-        public string DataEvento { get; set; }
+        public DateTime? DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public int UserId { get; set; }
+        public IEnumerable<Lote> Lote { get; set; }
+        public IEnumerable<RedeSocial> RedesSociais { get; set; }
+        public IEnumerable<PalestranteEvento> PalestranteEventos { get; set; }
     }
 }
